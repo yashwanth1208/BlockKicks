4e66bd8820624caa3101f13ed023dce9523aa425
Yashwanth Sai Sollu <www.sai1929@gmail.com>
2024-05-10 13:49:45 +0530

    defined models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yashwanth1208/BlockKicks-Authenticating-Sneakers-with-Blockchain-and-GoLang
 
 go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Block struct {
+	Pos       int
+	Data      SneakerCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
+}
+
+type SneakerCheckout struct {
+	SneakerID    string `json:"sneaker_id"`
+	Customer     string `json:"customer"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"` // Genesis is the intial block in a blockchain
+}
+
+type Sneaker struct {
+	ID              string `json:"id"`
+	Silhouette      string `json:"silhouette"`
+	Brand           string `json:"brand"`
+	ManufactureDate string `json:"manufacture_date"`
+	ISBN            string `json:"isbn"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var Blockchain *Blockchain
 
 func main() {
 	fmt.Println("BlockKicks")
+	r := mux.NewRouter()
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/new", newSneaker).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000...")
 }
